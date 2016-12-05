3120e9f71591d25e41ed0fe209b1cc352b0ea8e4
Antiklesys <syselkitna@gmail.com>
2016-12-05 11:43:48 +0100

    Change to the default bed temp for PLA
    Change to the default bed temp for PLA

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -591,7 +591,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of
 
 // Preheat Constants
 #define PLA_PREHEAT_HOTEND_TEMP 210 
-#define PLA_PREHEAT_HPB_TEMP 70
+#define PLA_PREHEAT_HPB_TEMP 60
 #define PLA_PREHEAT_FAN_SPEED 0		// Insert Value between 0 and 255
 
 #define ABS_PREHEAT_HOTEND_TEMP 240
